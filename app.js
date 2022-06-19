499c8af3d36d578785790857e19ed14018c78523
Mehin Melikova <mhmelikova@gmail.com>
2022-06-19 15:09:34 +0400

    css added.

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ const result_display=document.getElementById("result");
 
 const possible_choices=document.querySelectorAll("button");
 
+let img_width=computer_choice_display.clientWidth;
+computer_choice_display.style.minHeight=img_width+50+"px";
+
+
 let user_choice;
 let computer_choice;
 let result;
 
 possible_choices.forEach(possible_choice=>possible_choice.addEventListener("click", (e)=>{
     user_choice=e.target.id;
-    user_choice_display.innerHTML=user_choice;
-    generates_computer_choice();
-    get_result();
+    computer_choice_display.innerHTML="";
+    user_choice_display.innerHTML="";
+    result_display.innerHTML="";
+    setTimeout(()=>{
+        generates_computer_choice();
+        user_choice_display.innerHTML=`<img src="images/${user_choice}.png" alt="">`;
+    }, 200);
+    setTimeout(get_result, 300);
 }));
 
 function generates_computer_choice(){
@@ -26,30 +35,31 @@ function generates_computer_choice(){
     if(random_number===3){
         computer_choice='scissors';
     }
-    computer_choice_display.innerHTML=computer_choice;
+    computer_choice_display.innerHTML=`<img src="images/${computer_choice}.png" alt="">`;
 }
 
 function get_result(){
     if(user_choice===computer_choice){
-        result="It's a draw";
+        result="Draw";
     }
     if(computer_choice==='rock' && user_choice==="paper"){
-        result="You won";
+        result="Won";
     }
     if(computer_choice==='rock' && user_choice==="scissors"){
-        result="You lost";
+        result="Lost";
     }
     if(computer_choice==='paper' && user_choice==="rock"){
-        result="You lost";
+        result="Lost";
     }
     if(computer_choice==='paper' && user_choice==="scissors"){
-        result="You won";
+        result="Won";
     }
     if(computer_choice==='scissors' && user_choice==="paper"){
-        result="You lost";
+        result="Lost";
     }
     if(computer_choice==='scissors' && user_choice==="rock"){
-        result="You won";
+        result="Won";
     }
     result_display.innerHTML=result;
 }
+
